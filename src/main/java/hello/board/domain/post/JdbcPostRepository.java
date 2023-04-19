96d28c216fba6534dee5651574a6e2cbbfc4eea1
HyunSik Ryu <hsryu123@naver.com>
2023-04-19 23:24:24 +0900

    Jdbc 사용 시작 save까지 만듬

---
+++ src/main/java/hello/board/domain/post/JdbcPostRepository.java
@@ -0,0 +1,115 @@
+package hello.board.domain.post;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.support.JdbcUtils;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.List;
+@Slf4j
+//@Repository
+@RequiredArgsConstructor
+public class JdbcPostRepository implements PostRepository{
+    private final DataSource dataSource;
+    private static Long  sequence = 0L;
+
+    @Override
+    public Post save(Post post) {
+        post.setDate(LocalDate.now());
+        post.setId(++sequence);
+        java.sql.Date sqlDate = java.sql.Date.valueOf(post.getDate());
+        String sql = "insert into post(writerId, title, content, date, views) values (?, ?, ?, ?, ?)";
+
+        Connection con = null; //커넥션
+        PreparedStatement pstmt = null; // db에 쿼리를 날리는 도구
+
+        try {
+            con = getConnection(); // 커넥션 획득 메소드
+            pstmt = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            //파라미터 바인딩
+            //pstmt.setLong(1, post.getId());
+            pstmt.setString(1,post.getWriterId());
+            pstmt.setString(2,post.getTitle());
+            pstmt.setString(3,post.getContent());
+            pstmt.setDate(4,sqlDate);
+            pstmt.setLong(5,post.getViews());
+
+            pstmt.executeUpdate();
+
+            return post;
+        } catch (SQLException e) {
+            log.error("db error", e);
+
+        } finally{
+            close(con,pstmt,null);
+        }
+        return post;
+    }
+
+    @Override
+    public Post findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Post> findByWriterId(String writerId) {
+        return null;
+    }
+
+    @Override
+    public List<Post> findByTitle(String title) {
+        return null;
+    }
+
+    @Override
+    public List<Post> findByContents(String content) {
+        return null;
+    }
+
+    @Override
+    public List<Post> findByTitleAndContent(String word) {
+        return null;
+    }
+
+    @Override
+    public List<Post> findAll() {
+        return null;
+    }
+
+    @Override
+    public void updatePost(Long id, Post updateParam) {
+
+    }
+
+    @Override
+    public void addView(Post post) {
+
+    }
+
+    @Override
+    public void deletePost(Long id) {
+
+    }
+
+    /**
+     * pstmt에서 exception이 발생하면 con.close() 가 호출이 안될수도 있다.
+     * 따라서 exception이 발생해도 마저 close 하도록 설정한다.
+     */
+    private void close(Connection con, Statement stmt, ResultSet rs){
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+        JdbcUtils.closeConnection(con);
+    }
+    private Connection getConnection() throws SQLException {
+        Connection con = dataSource.getConnection();
+        log.info("get connection={}, class={}", con, con.getClass());
+        return con;
+    }
+    @Override
+    public void clearStore() {
+
+    }
+}
---
+++ src/test/java/hello/board/domain/post/JdbcPostRepositoryTest.java
@@ -0,0 +1,35 @@
+package hello.board.domain.post;
+
+import com.zaxxer.hikari.HikariDataSource;
+import hello.board.domain.dbconnection.ConnectionConst;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static hello.board.domain.dbconnection.ConnectionConst.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class JdbcPostRepositoryTest {
+
+    PostRepository postRepository;
+    @BeforeEach
+    void beforeEach(){
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        postRepository = new JdbcPostRepository(dataSource);
+    }
+    @Test
+    void save() {
+        Post post1 = new Post("test", "findByWriterId-test1", "content-findByWriterId1");
+        Post post2 = new Post("test", "findByWriterId-test2", "content-findByWriterId2");
+
+        postRepository.save(post1);
+        postRepository.save(post2);
+
+    }
+
+
+}
